39f79a0f3d0a68a81c79178b07b339f745aeea54
RhenzoHideki <rhskgames@gmail.com>
2020-03-11 09:40:50 -0300

    Separador de strings

--- fila_substrings.cpp
+++ fila_substrings.cpp
@@ -1,1 +1,17 @@
 // implemente aqui sua função
+
+#include "fila_substrings.h"
+
+void separa(string algo, char sep, queue<string> & q){
+    if (!algo.empty()){
+        size_t agora , anterior = 0;
+        agora = algo.find(sep);
+         while (agora!=algo.npos)
+            {
+             q.push(algo.substr(anterior,agora-anterior));
+             anterior=agora+1;
+             agora = algo.find(sep,anterior);
+            }
+        q.push(algo.substr(anterior,agora-anterior));
+    }
+}
--- fila_substrings.h
+++ fila_substrings.h
@@ -1,8 +1,22 @@
 //
 // Created by msobral on 29/07/2019.
 //
-
 #ifndef FILA_SUBSTRINGS_H
+
+#pragma once
+
+#include <queue>
+#include <string>
+#include <iostream>
+
+#include "fila_substrings.h"
+
+using namespace std;
+
+
+void separa(string algo, char sep, queue<string> & q);
+
+
 #define FILA_SUBSTRINGS_H
 
 
